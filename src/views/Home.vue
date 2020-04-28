b2f092c91a00466e3add35c09fc1c1113ca8321a
Firman <fblazt@outlook.com>
2020-04-28 21:25:56 +0700

    Ready to go

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen w-full flex flex-row justify-center items-center">
+    <p class="h-64 w-64 bg-blue-900 text-white">
+      Saylo
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
